25522ba91de0e8a033916fd05141ea988f8db643
gblinux <gabrielfilosofiaprofessor@gmail.com>
2020-12-13 12:16:22 -0400

    Implementa metodo reduce com laco

--- src/ContaHashtagReducer.java
+++ src/ContaHashtagReducer.java
@@ -1,2 +1,20 @@
-public class ContaHashtagReducer {
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class ContaHashtagReducer extends Reducer<Text, IntWritable, Text, IntWritable>
+{
+    private IntWritable resultado = new IntWritable();
+
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
+    {
+        int soma = 0;
+        for(IntWritable val : values){
+            soma += val.get();
+        }
+        resultado.set(soma);
+        context.write(key, resultado);
+    }
 }
